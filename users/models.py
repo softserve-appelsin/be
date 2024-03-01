02acba5ab952b6ae9ae2890b781403ed038cf133
Жимас <zhymas@MacBook-Pro-Zimas.local>
2024-03-01 22:02:53 +0200

    create models for user, create new app main

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    PROFILE_TYPES = (
+        ('Artist', 'Artist'),
+        ('Listener', 'Listener')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_type = models.CharField(max_length=100, choices=PROFILE_TYPES)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(blank=True)
