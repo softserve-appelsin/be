f03ba29a2c04ae8423e1041bca1df46b68c8a4bf
Жимас <zhymas@MacBook-Pro-Zimas.local>
2024-03-02 22:24:36 +0200

    add jwt

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import CreateUserAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', CreateUserAPIView.as_view())
+    path('api/v1/auth/token', TokenObtainPairView.as_view()),
+    path('api/v1/auth/refresh', TokenRefreshView.as_view()),
+    path('api/v1/create_user', CreateUserAPIView.as_view())
 ]
