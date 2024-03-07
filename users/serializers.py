8c989af0da322a5d443545ba697493a47c2edc8d
Жимас <zhymas@MacBook-Pro-Zimas.local>
2024-03-07 22:46:56 +0200

    update auth, add new field for profile, add new required fields for registrations

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,11 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
+        extra_kwargs = {
+            'email': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
 
 class UserProfileSerializer(ModelSerializer):
     class Meta:
