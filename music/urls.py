644ec4b1451058ba5f803166280df0c73db68a08
Khrystyna29 <144240193+Khrystyna29@users.noreply.github.com>
2024-03-30 15:16:18 +0200

    API for show all info (#42)

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TrackAPIView, CreateTrackAPIView, PlayListAPIView
+from .views import TrackAPIView, CreateTrackAPIView, PlayListAPIView, PlayListInfoAPIView
 
 urlpatterns = [
     path('tracks/', TrackAPIView.as_view()),
     path('create_track/', CreateTrackAPIView.as_view()),
-    path('playlists/', PlayListAPIView.as_view())
-]
+    path('playlists/', PlayListAPIView.as_view()),
+    path('playlists_info/', PlayListInfoAPIView.as_view())]
