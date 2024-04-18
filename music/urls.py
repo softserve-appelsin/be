b18f87d1a6f0196e4fe8a823ad9f5c11f6e61ded
zhymas <zhymas@MacBook-Pro-Zimas.local>
2024-04-18 18:25:39 +0300

    fix deploy

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TrackAPIView, CreateTrackAPIView, PlayListAPIView, PlayListInfoAPIView
+from .views import TrackAPIView, PlayListAPIView, PlayListInfoAPIView
 
 urlpatterns = [
     path('tracks/', TrackAPIView.as_view()),
-    path('create_track/', CreateTrackAPIView.as_view()),
     path('playlists/', PlayListAPIView.as_view()),
     path('playlists_info/', PlayListInfoAPIView.as_view())
 ]
