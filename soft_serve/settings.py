1d172f7dcb0b7b8974967707d2e6726d8335fdae
zhymas <zhymas@MacBook-Pro-Zimas.local>
2024-05-04 15:52:22 +0300

    fix stage

--- soft_serve/settings.py
+++ soft_serve/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'djoser',
     'music.apps.MusicConfig',
-    'user_info.apps.UserInfoConfig',
+    'user_info.apps.UserInfoConfig'
     'storages'
 ]
 
@@ -154,8 +154,8 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -165,9 +165,9 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
 LANGUAGE_CODE = 'en-us'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'soft_serve/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'soft_serve/static'),
+# ]
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -182,7 +182,7 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+# DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
 
 TIME_ZONE = 'UTC'
 
