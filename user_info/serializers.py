e9af7edda8ea6162eaa9be48ec5b1241a8d5aa70
zhymas <zhymas@MacBook-Pro-Zimas.local>
2024-05-08 22:59:05 +0300

    create serializers for user_info

---
+++ user_info/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from users.models import Profile
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['id', 'last_login', 'username', 'first_name', 'last_name', 'date_joined']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Profile
+        fields = ['bio']
